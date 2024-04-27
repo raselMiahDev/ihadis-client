faceba3d73f921c3c89c57761e13563c4cc07f7e
Rasel Kibria <mdraselkibria11@gmail.com>
2024-04-28 00:48:29 +0600

    build project

--- src/app/bukhari/page.js
+++ src/app/bukhari/page.js
@@ -1,17 +1,34 @@
 "use client";
 import Breadcrumb from "@/components/Breadcrumb";
 import Card from "@/components/Card";
-import React from "react";
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
 const page = () => {
+  const [hadiths, setHadiths] = useState([]);
+
+  useEffect(() => {
+    // get hadiths from api
+    const fetchHadith = async () => {
+      try {
+        const response = await fetch(
+          "https://ihadis-server.onrender.com/hadith"
+        );
+        const data = await response.json();
+        setHadiths(data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchHadith();
+  }, []);
   const currentRoute = usePathname();
   return (
-    <div className="overflow-auto min-h-[30rem] md:min-h-[20rem] lg:min-h-[1/3] xl:min-h-[1/3] 2xl:min-h-[1/3]">
+    <div className="overflow-auto h-[500px]">
       <div className="bg-slate-50 p-3 rounded-t-lg">
         <Breadcrumb currentRoute={currentRoute} />
       </div>
       <hr />
-      <Card />
+      <Card data={hadiths} />
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
+import Link from "next/link";
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-[100vh]">
-      <h1>Home</h1>
+    <div className="flex justify-center items-center h-[90vh] bg-gray-100">
+      <Link
+        href="/bukhari"
+        className="bg-green-700 px-16 py-3 hover:bg-transparent border hover:border-green-800 hover:text-green-800 transition hover:duration-300 text-white rounded-lg flex gap-3 items-center text-lg shadow-md"
+      >
+        <span>হাদিস পড়ি</span>
+        <span>
+          <FaArrowRightLong />
+        </span>
+      </Link>
     </div>
   );
 };
